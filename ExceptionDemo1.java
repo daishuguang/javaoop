4fae8cc6cf2cc869e5ec24e78da128d6b121e61b
daishuguang <daishuguang4461@126.com>
2014-09-27 21:03:33 +0800

    commit

---
+++ ExceptionDemo1.java
@@ -0,0 +1,12 @@
+public class ExceptionDemo1 {
+	public static void main(String args[]) {
+		System.out.println("**************begin ******");
+		/*
+		int i = 0;
+		int j = 0;
+		int temp = i / j;
+		System.out.println("result: " + temp);
+		System.out.println("****************end *************");
+		*/
+	}
+}
